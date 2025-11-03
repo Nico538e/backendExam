9bcbc9ad99775e76e82f101c97b773cf0ca10aff
Nico538e <nicolai-strand@hotmail.com>
2025-11-03 13:10:16 +0100

    Initial commit

---
+++ src/main/java/app/routes/Routes.java
@@ -0,0 +1,16 @@
+package app.routes;
+
+import io.javalin.apibuilder.EndpointGroup;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
+
+public class Routes {
+    public EndpointGroup getRoutes() {
+        return () -> {
+            get("/", ctx -> ctx.result("Hello api"));
+
+
+        };
+    }
+}
